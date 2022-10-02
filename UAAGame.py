89cae3d766e7b158d067fecd92bb5ac6da9b359f
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-10-01 22:25:16 -0800

    refactored screen setup into separate code, made a function

--- UAAGame.py
+++ UAAGame.py
@@ -3,32 +3,13 @@ from pygame.locals import *
 import os
 import math
 import random
+from screen_setup import initialize_screen
 
 from SpriteSheet import SpriteSheet, scale_image, draw_text
 from World import World, Tile
 
-# game window
-pygame.init()
-SCREEN_WIDTH = int(pygame.display.Info().current_w / 1.5)
-SCREEN_HEIGHT = int(pygame.display.Info().current_h / 1.5)
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), RESIZABLE)
-fullscreen = False
-# These store the width and height for switching between full screen and windowed.
-setScreenWidth = SCREEN_WIDTH
-setScreenHeight = SCREEN_HEIGHT
-'''
-DO NOT REMOVE 
-Setting the mode twice ensures the VIDEORESIZE event is called when resizing the screen for the first time.
-Without this the window will not resize properly when the user changes its size for the first time.
-'''
-pygame.display.set_mode((0,0), FULLSCREEN | RESIZABLE)
-pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), RESIZABLE)
+screen, SCREEN_WIDTH, SCREEN_HEIGHT = initialize_screen(two_monitors=False)
 
-if(False): # Set this to true if you want it to appear on a second monitor
-    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (-SCREEN_WIDTH,32)
-
-# Title
-pygame.display.set_caption('UAA Game')
 # Icon
 toddler = pygame.image.load("screaming_toddler.jpg")
 pygame.display.set_icon(toddler)
---
+++ screen_setup.py
@@ -0,0 +1,41 @@
+"""
+Basic code to initialize pygame, I'm a little worried about the potential for circular imports being an issue,
+but we'll cross that bridge when we come to it.
+"""
+
+import pygame
+from pygame.locals import RESIZABLE, FULLSCREEN
+from os import environ
+
+def initialize_screen(two_monitors=False,
+                      game_title="Uaa Game") -> tuple[pygame.display, int, int]:
+    """
+    this function initializes pygame and sets up the main window.
+
+    """
+
+    # initialize game window
+    pygame.init()
+    SCREEN_WIDTH = int(pygame.display.Info().current_w / 1.5)
+    SCREEN_HEIGHT = int(pygame.display.Info().current_h / 1.5)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), RESIZABLE)
+    fullscreen = False
+    # These store the width and height for switching between full screen and windowed.
+    setScreenWidth = SCREEN_WIDTH
+    setScreenHeight = SCREEN_HEIGHT
+    '''
+    DO NOT REMOVE 
+    Setting the mode twice ensures the VIDEORESIZE event is called when resizing the screen for the first time.
+    Without this the window will not resize properly when the user changes its size for the first time.
+    '''
+    pygame.display.set_mode((0, 0), FULLSCREEN | RESIZABLE)
+    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), RESIZABLE)
+
+    if two_monitors:  # Set this to true if you want it to appear on a second monitor
+        environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (-SCREEN_WIDTH, 32)
+
+    # Title
+    pygame.display.set_caption(game_title)
+
+    # after the screen is configured, return a tuple with the screen object, the width and the height
+    return screen, SCREEN_WIDTH, SCREEN_HEIGHT
